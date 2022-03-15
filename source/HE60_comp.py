5dc060f732197f30c4abafe7a172344b1475dd9a
Bastian Raulier <braulier@Disquette.local>
2022-03-15 13:31:50 -0400

    Building HE60_comp.py to prepare comparison

--- source/HE60_comp.py
+++ source/HE60_comp.py
@@ -4,6 +4,9 @@ from geometric_rolloff import OpenMatlabFiles
 from processing import ProcessImage, FigureFunctions
 import pandas
 
+from HE60PY.ac9simulation import AC9Simulation
+
+
 
 # Function and classes
 def open_TriOS_data(min_date="2018-08-31 8:00", max_date="2018-08-31 16:00", path_trios="data/2018R4_300025060010720.mat"):
@@ -35,4 +38,13 @@ if __name__ == "__main__":
     # ze_mesh, az_mesh, rad_profile = process.open_radiance_data(path="data/oden-08312018.h5")
     # print(az_mesh, '\n', ze_mesh)
     time_stamp, radiometer_wl, irr_incom, irr_below = open_TriOS_data()
+
+
+    path_to_user_files = r'/Users/braulier/Documents/HE60/run'
+    wavelength_abs_built_sim = AC9Simulation(path=path_to_user_files, run_title='He60_dort_comp', root_name='he60 comparison to dort', mode='sea_ice')
+    wavelength_abs_built_sim.set_z_grid(z_max=2.0, wavelength_list=[484, 544, 602])
+    wavelength_abs_built_sim.add_layer(z1=0.0, z2=0.10, abs={'484': 0.0430, '544': 0.0683, '602': 0.12}, scat=2277, bb=0.0109)
+    wavelength_abs_built_sim.add_layer(z1=0.10, z2=0.80, abs={'484': 0.0430, '544': 0.0683, '602': 0.12}, scat=303, bb=0.0042)
+    wavelength_abs_built_sim.add_layer(z1=0.80, z2=2.01, abs={'484': 0.0430, '544': 0.0683, '602': 0.12}, scat=79, bb=0.0042)
+    wavelength_abs_built_sim.run_simulation(printoutput=True)
 
